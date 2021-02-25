dcb248be4fe66ca1d20a872a7cb935e5b2b02225
OlafurjonHI <ojv1@hi.is>
2021-02-25 14:34:29 +0000

    login code virkar

--- nafnaneistarApp/app/src/main/java/xyz/nafnaneistar/loginactivity/LoginActivity.java
+++ nafnaneistarApp/app/src/main/java/xyz/nafnaneistar/loginactivity/LoginActivity.java
@@ -1,29 +1,63 @@
 package xyz.nafnaneistar.loginactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import xyz.nafnaneistar.loginactivity.controller.ApiController;
+import androidx.databinding.DataBindingComponent;
+import androidx.databinding.DataBindingUtil;
 
+import xyz.nafnaneistar.loginactivity.controller.ApiController;
+import xyz.nafnaneistar.loginactivity.data.User;
+import xyz.nafnaneistar.loginactivity.databinding.ActivityLoginBinding;
+
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.android.volley.Request;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
 
 public class LoginActivity extends AppCompatActivity {
-    String testUrl = "http://192.168.1.207:7979/login/check?email=test@test.is&password=tt";
+    private ActivityLoginBinding binding;
+
     String test2Url = "http://nafnaneistar.xyz/swipe/newname";
+    private Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login);
+        binding.btnLogin.setOnClickListener(view1 -> {
 
-        JsonObjectRequest jsonArrReq = new JsonObjectRequest(Request.Method.GET,testUrl,null,
-                response -> {
-                    Log.d("test",   "onCreate: " + response.toString());
-                },error -> {
-                    Log.d("test",   "onCreate: " + error.toString());
+            CheckLogin(view1);
         });
 
-        ApiController.getInstance().addToRequestQueue(jsonArrReq);
+    }
+
+    public void CheckLogin(View view,String url){
+        Log.d("Test", "CheckLogin: Button Clicked");
+        String email = binding.etEmail.getText().toString().trim();
+        String pass = binding.etPassword.getText().toString().trim();
+        String loginUrl = "http://192.168.1.207:7979/login/check?email="+email+"&password="+pass;
+        JsonObjectRequest jsonArrReq = new JsonObjectRequest(Request.Method.GET,url,null,
+                response -> {
+                    Log.d("Test", "CheckLogin: " + response.toString());
+                    Gson g = new Gson();
+                    User p = g.fromJson(String.valueOf(response), User.class);
+                    Log.d("Test", "CheckLogin: " + p.getName());
+                    if(p.getName()){
+                        binding.tvError.setText("Innskráning Tókst!");
+                    }
+                    else {
+                        binding.tvError.setText("Innskráning Mistókst!");
+                    }
+                },error -> {
+                binding.tvError.setText("Innskráning Mistókst!");
+                Log.d("Test", "CheckLogin: " + error.toString());
+        });
+            ApiController.getInstance().addToRequestQueue(jsonArrReq);
+
     }
 }
---
+++ nafnaneistarApp/app/src/main/java/xyz/nafnaneistar/loginactivity/data/User.java
@@ -0,0 +1,80 @@
+package xyz.nafnaneistar.loginactivity.data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class User {
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+    private ArrayList<Integer> availableNames;
+    private HashMap<Integer, Integer> approvedNames;
+    private ArrayList<Long> linkedPartners;
+
+    public User(long id, String name, String email, String password, ArrayList<Integer> availableNames, HashMap<Integer, Integer> approvedNames, ArrayList<Long> linkedPartners) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.availableNames = availableNames;
+        this.approvedNames = approvedNames;
+        this.linkedPartners = linkedPartners;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ArrayList<Integer> getAvailableNames() {
+        return availableNames;
+    }
+
+    public void setAvailableNames(ArrayList<Integer> availableNames) {
+        this.availableNames = availableNames;
+    }
+
+    public HashMap<Integer, Integer> getApprovedNames() {
+        return approvedNames;
+    }
+
+    public void setApprovedNames(HashMap<Integer, Integer> approvedNames) {
+        this.approvedNames = approvedNames;
+    }
+
+    public ArrayList<Long> getLinkedPartners() {
+        return linkedPartners;
+    }
+
+    public void setLinkedPartners(ArrayList<Long> linkedPartners) {
+        this.linkedPartners = linkedPartners;
+    }
+}
