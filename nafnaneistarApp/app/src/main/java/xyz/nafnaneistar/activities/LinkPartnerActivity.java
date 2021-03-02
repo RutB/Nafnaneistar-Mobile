0e9c65a9907399163d61f5eb8263283a9af986fc
RutB <42755206+RutB@users.noreply.github.com>
2021-03-02 21:05:09 +0000

    linkPartner byrjun

---
+++ nafnaneistarApp/app/src/main/java/xyz/nafnaneistar/activities/LinkPartnerActivity.java
@@ -0,0 +1,20 @@
+package xyz.nafnaneistar.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import xyz.nafnaneistar.loginactivity.R;
+import xyz.nafnaneistar.loginactivity.databinding.ActivityLoginBinding;
+import xyz.nafnaneistar.model.User;
+
+public class LinkPartnerActivity extends AppCompatActivity {
+    private ActivityLinkBinding binding;
+    private User currentUser = new User();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_link_partner);
+    }
+}
