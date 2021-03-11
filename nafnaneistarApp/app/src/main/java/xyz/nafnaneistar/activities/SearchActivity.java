1aa857863b49a1c1273c6a42477eadca4a023d9c
Hafthorat <sterinn@internet.is>
2021-03-11 12:35:23 +0000

    Ekkert að gerast yet

--- nafnaneistarApp/app/src/main/java/xyz/nafnaneistar/activities/SearchActivity.java
+++ nafnaneistarApp/app/src/main/java/xyz/nafnaneistar/activities/SearchActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import xyz.nafnaneistar.loginactivity.R;
+
 public class SearchActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +13,6 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
     }
+
+
 }
