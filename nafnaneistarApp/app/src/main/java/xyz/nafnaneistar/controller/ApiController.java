e675016df04ec96269c79f9a55291cf78edde59a
RutB <42755206+RutB@users.noreply.github.com>
2021-04-07 22:17:56 +0000

    Reyna að láta fragment virka með callback

--- nafnaneistarApp/app/src/main/java/xyz/nafnaneistar/controller/ApiController.java
+++ nafnaneistarApp/app/src/main/java/xyz/nafnaneistar/controller/ApiController.java
@@ -35,8 +35,8 @@ import xyz.nafnaneistar.model.User;
  */
 public class ApiController extends Application {
     private static ApiController instance;
-    //private static String domainURL = "http://46.22.102.179:7979/";
-    private static String domainURL = "http://192.168.1.207:7979/";
+    private static String domainURL = "http://46.22.102.179:7979/";
+    //private static String domainURL = "http://192.168.1.207:7979/";
     //private static String domainURL = "localhost:7979/";
     // private static String domainURL = "http://127.0.0.1:7979/";
     //private static String domainURL = "http://192.168.0.164:7979/";
@@ -388,7 +388,7 @@ public class ApiController extends Application {
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                    volleyCallBack.onResponse((ArrayList<UserItem>) partnersList);
+                    volleyCallBack.onUserResponse((ArrayList<UserItem>) partnersList);
                 }, error -> {
             volleyCallBack.onError( getString(R.string.errorGettingPartners));
         });
